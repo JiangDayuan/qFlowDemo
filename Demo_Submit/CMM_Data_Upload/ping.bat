1bdf81ab81b33eb40a561d6545581bb071b5539b
MattZhao <zhaojiaqing1991@163.com>
2019-07-10 20:52:07 +0800

    更新上传代码和模板 by江丰元

--- Demo_Submit/CMM_Data_Upload/ping.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-::set ip = 10.202.0.9
-::set lip=10.202.120.59
-::Ping.exe -n 1 %ip% >> status.txt
-::Ping.exe -n 1 %lip% >> status.txt
--- Demo_Submit/CT_Data_Upload/ping.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-::set ip = 10.202.0.9
-::set lip=10.202.0.9
-::Ping.exe -n 1 %ip% >> status.txt
-::Ping.exe -n 1 %lip% >> status.txt
